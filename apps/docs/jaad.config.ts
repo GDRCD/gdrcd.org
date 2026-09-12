b4dea55ec9f77035491a7d1999c2588c5f68dff2
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-09-12 09:21:34 +0200

    upd: aggiornati colori docs

--- apps/docs/jaad.config.ts
+++ apps/docs/jaad.config.ts
@@ -1,5 +1,33 @@
 import { defineJaadConfig } from "@lancher-dev/jaad";
 
+const GOOGLE_FONTS =
+  "https://fonts.googleapis.com/css2" +
+  "?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,400" +
+  "&family=JetBrains+Mono:wght@400;600" +
+  "&display=swap";
+
 export default defineJaadConfig({
   title: "GDRCD",
+  lang: "it",
+  logo: "/brand/gdrcd-wordmark.png",
+
+  social: { github: "https://github.com/GDRCD/GDRCD" },
+
+  theme: { light: "vitesse-light", dark: "vitesse-dark" },
+
+  head: [
+    {
+      tag: "link",
+      attrs: { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    },
+    {
+      tag: "link",
+      attrs: {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: "anonymous",
+      },
+    },
+    { tag: "link", attrs: { rel: "stylesheet", href: GOOGLE_FONTS } },
+  ],
 });
